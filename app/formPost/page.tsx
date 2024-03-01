88e394d3fab792cbe8d3e16951cf6e8ceb83b320
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-01 14:45:54 +0300

    Made some markup changes to the pages

--- app/formPost/page.tsx
+++ app/formPost/page.tsx
@@ -1,7 +1,41 @@
-type Props = {}
+import { revalidatePath } from 'next/cache'
 
-const FormPostPage = (props: Props) => {
-	return <div>FormPostPage</div>
+const todos: string[] = ['Impala']
+
+const FormPostPage = () => {
+	const addTodo = async (data: FormData) => {
+		'use server'
+
+		const todo = data.get('todo') as string
+
+		todos.push(todo)
+
+		revalidatePath('/')
+	}
+
+	return (
+		<main className='min-h-screen p-24'>
+			<h1 className='text-4xl font-bold'>Todos</h1>
+			<ul>
+				{todos.map((todo, index) => {
+					return <li key={index}>{todo}</li>
+				})}
+			</ul>
+			<form action={addTodo}>
+				<input
+					type='text'
+					name='todo'
+					className='border border-gray-300 rounded-lg py-4 px-4 text-base text-black'
+				/>
+				<button
+					type='submit'
+					className='bg-blue-600 disabled:bg-gray-500 inline-flex rounded-md p-2 ml-3'
+				>
+					Submit
+				</button>
+			</form>
+		</main>
+	)
 }
 
 export default FormPostPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,9 @@
-import { revalidatePath } from "next/cache"
-
-const todos: string[] = ['Impala']
+import Link from 'next/link'
 
 export default function Home() {
-	const addTodo = async (data: FormData) => {
-		'use server'
-
-		const todo = data.get('todo') as string
-
-		todos.push(todo)
-
-    revalidatePath('/')
-	}
-
 	return (
-		<main className='min-h-screen p-24'>
-			<h1 className='text-4xl font-bold'>Todos</h1>
-			<ul>
-				{todos.map((todo, index) => {
-					return <li key={index}>{todo}</li>
-				})}
-			</ul>
-			<form action={addTodo}>
-				<input
-					type='text'
-					name='todo'
-					className='border border-gray-300 rounded-lg py-4 px-4 text-base text-black'
-				/>
-				<button
-					type='submit'
-					className='bg-blue-600 disabled:bg-gray-500 inline-flex rounded-md p-2 ml-3'
-				>
-					Submit
-				</button>
-			</form>
+		<main className='min-h-screen'>
+			<Link href='/formPost'>Form Post</Link>
 		</main>
 	)
 }
