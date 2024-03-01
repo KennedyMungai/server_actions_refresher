e73d99b34963b588742e28a5bbd8c8505f36c3b3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-01 15:01:45 +0300

    Made some hierarchical changes

--- app/formPost/addButton.tsx
+++ app/formPost/addButton.tsx
@@ -3,20 +3,27 @@ import React from 'react'
 import { useFormStatus } from 'react-dom'
 
 type Props = {
-    addTodo: (todo: string) => {}
+	addTodo: (todo: string) => {}
 }
 
-const AddButton = ({addTodo}: Props) => {
+const AddButton = ({ addTodo }: Props) => {
 	const { pending } = useFormStatus()
 
 	return (
-		<button
-			type='submit'
-			disabled={pending}
-			className='bg-blue-600 disabled:bg-gray-500 inline-flex rounded-md p-2 ml-3'
-		>
-			Submit
-		</button>
+		<>
+			<input
+				type='text'
+				name='todo'
+				className='border border-gray-300 rounded-lg py-4 px-4 text-base text-black'
+			/>
+			<button
+				type='submit'
+				disabled={pending}
+				className='bg-blue-600 disabled:bg-gray-500 inline-flex rounded-md p-2 ml-3'
+			>
+				Submit
+			</button>
+		</>
 	)
 }
 
--- app/formPost/page.tsx
+++ app/formPost/page.tsx
@@ -23,11 +23,7 @@ const FormPostPage = () => {
 				})}
 			</ul>
 			<div>
-				<input
-					type='text'
-					name='todo'
-					className='border border-gray-300 rounded-lg py-4 px-4 text-base text-black'
-				/>
+				
 				<AddButton addTodo={addTodo} />
 			</div>
 		</main>
