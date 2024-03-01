e25156d9aebffa001b9169cd48a7d995d53631b2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-01 15:30:02 +0300

    Added a useEffect for calling the search on the pokemon

--- app/pokemonSearch/pokemonList.tsx
+++ app/pokemonSearch/pokemonList.tsx
@@ -1,12 +1,16 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 type Props = {
-    search: (search: string) => Promise<string[]>
+	search: (search: string) => Promise<string[]>
 }
 
-const PokemonList = (props: Props) => {
+const PokemonList = ({ search }: Props) => {
 	const [pokemonNames, setPokemonNames] = useState<string[]>([])
+
+	useEffect(() => {
+		search('').then((names) => setPokemonNames(names))
+	}, [search])
 
 	return <div>PokemonList</div>
 }
