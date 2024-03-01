668390938c45fc8be7f852f066f5a19451f04de0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-01 14:56:03 +0300

    Disabled the button when submission is pending

--- app/formPost/addButton.tsx
+++ app/formPost/addButton.tsx
@@ -1,12 +1,16 @@
 'use client'
 import React from 'react'
+import { useFormStatus } from 'react-dom'
 
 type Props = {}
 
 const AddButton = (props: Props) => {
+	const { pending } = useFormStatus()
+
 	return (
 		<button
 			type='submit'
+            disabled={pending}
 			className='bg-blue-600 disabled:bg-gray-500 inline-flex rounded-md p-2 ml-3'
 		>
 			Submit
