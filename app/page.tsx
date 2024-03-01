260d3c2003d4f185bf37b49195d689ec97da4ed8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-01 14:16:26 +0300

    Printed out the todos

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+const todos: string[] = []
+
 export default function Home() {
-	return <main className='min-h-screen'></main>
+	return (
+		<main className='min-h-screen p-24'>
+			<h1 className='text-4xl font-bold'>Todos</h1>
+			<ul>
+				{todos.map((todo, index) => {
+					return <li key={index}>{todo}</li>
+				})}
+			</ul>
+		</main>
+	)
 }
